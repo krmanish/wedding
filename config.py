5064c5b2420b0453d37999d42a705434b88ad85a
Manish Kumar <krmanish260@gmail.com>
2020-03-08 13:40:19 +0530

    Heroku DB setup

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import os
+
 DEBUG = True
 
+# export DATABASE_URL = f'{DB.type}://{DB.username}:{DB.password}@{DB.host}/{DB.name}'
 
 TEMPLATES_DIRNAME = 'templates'
 
@@ -13,10 +16,11 @@ class DB:
 
 class AppConfig(object):
     DEBUG = DEBUG
+    CSRF_ENABLED = True
     SECRET_KEY = 'This is mahi first wedding project... Use it w1s3ly.'
     USER_APP_NAME = "Festival On"
     USER_ENABLE_EMAIL = True
     USER_ENABLE_USERNAME = True
     USER_REQUIRE_RETYPE_PASSWORD = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = f'{DB.type}://{DB.username}:{DB.password}@{DB.host}/{DB.name}'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
